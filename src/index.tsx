ce38539956fea73cd35060318063cf442e9912b1
liuxuanlong <liuxuanlong@baijia.com>
2022-12-14 11:44:20 +0800

    feat: 处理浏览器兼容

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '@/index.less'
+import '@/index.less';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
